6ef440a7c515f306a053a78ce3dded99d54bb581
Nathan Podesta <43050214+NathanPo@users.noreply.github.com>
2021-05-27 22:51:29 +0200

    Add live badges

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # maven_training
 ![example workflow](https://github.com/NathanPo/maven_training/actions/workflows/build.yml/badge.svg)
+[![codecov](https://codecov.io/gh/NathanPo/maven_training/branch/main/graph/badge.svg?token=LG6LG2BVEQ)](https://codecov.io/gh/NathanPo/maven_training)
--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -1,33 +1,34 @@
 package fr.lernejo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class SampleTest {
     static final Sample sample = new Sample();
 
     @Test
     public void negativeNumber () {
-        assertThrows(IllegalArgumentException.class, () -> new Sample().fact(-5));
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new Sample().fact(-5));
     }
 
     @Test
     public void addFiveOp() {
         int x = 4;
         int y = 1;
-        assertEquals(5, sample.op(Sample.Operation.ADD, x,y));
+        Assertions.assertThat(5).isEqualTo(sample.op(Sample.Operation.ADD, x,y));
     }
 
     @Test
     public void mulHeightOp() {
         int x = 4;
         int y = 2;
-        assertEquals(8, sample.op(Sample.Operation.MULT, x,y));
+        Assertions.assertThat(8).isEqualTo(sample.op(Sample.Operation.MULT, x,y));
     }
 
     @Test
     public void factZero() {
         int x = 0;
-        assertEquals(1, sample.fact(x));
+        Assertions.assertThat(1).isEqualTo(sample.fact(x));
     }
 }
